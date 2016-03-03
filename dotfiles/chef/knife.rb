e6ac8528b52643a6d0d7acb50b2ee25df2212b5c
Kei Kawano <kawa2587@yomiuri.com>
2016-03-03 16:05:43 +0900

    initial

---
+++ dotfiles/chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'whize'
+client_key               '/Users/whize/.chef/whize.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://whize-MacBookAir.local:443'
+syntax_check_cache_path  '/Users/whize/.chef/syntax_check_cache'
